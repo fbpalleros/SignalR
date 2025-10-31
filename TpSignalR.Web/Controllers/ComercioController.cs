63a8c601573abfbda6e4c0e202ca02c1f827b541
Facundo Palleros <facupls10@gmail.com>
2025-10-30 21:24:26 -0300

    agregando comercio controller

---
+++ TpSignalR.Web/Controllers/ComercioController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TpSignalR.Web.Controllers
+{
+    public class ComercioController : Controller
+    {
+        public IActionResult Inicio()
+        {
+            return View();
+        }
+
+        //
+        
+    }
+}
